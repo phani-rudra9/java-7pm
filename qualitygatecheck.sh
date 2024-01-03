51052cd9dd912dc5f1faf50c94dde09ead493d98
phani-rudra9 <phaninani12374@gmail.com>
2024-01-03 13:57:10 +0000

    sample commit

--- qualitygatecheck.sh
+++ qualitygatecheck.sh
@@ -3,7 +3,7 @@ curl -u admin:phani http://54.177.239.231:9000/api/qualitygates/project_status?p
 scan_status=$(cat check.json | grep -i status | cut -d ":" -f 3 | cut -d "," -f 1 | tr -d '"')
 if [[ $scan_status -eq OK ]]
 then
-  echo "sonar scan status is $scan_status........!!!"
+  echo "sonar scan status is $scan_status.............!!!"
 else
   echo "Sonar scan status is $scan_status please check ......!"
   exit 1;
